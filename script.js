7bb39b44f7a9788454cf9176a305b9acec6ba34c
BegayimU <b.usubakunova@gmail.com>
2025-10-24 17:04:34 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+let mark = prompt ('1ден 5ке чейин баа жаз')
+switch (mark) {
+    case ('1'):
+        alert('Эн начар')        
+        break;
+    case ('2'):
+        alert('Начар')
+        break;
+    case ('3'):  
+        alert('Канагаттандырылган')
+        break;
+    case ('4'):
+        alert('Жакшы')
+        break;
+    case ('5'):  
+        alert('Мыкты')
+        break;           
+    default:
+        alert('Туура жаз!')
+        break;
+}
